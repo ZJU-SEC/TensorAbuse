f650d912b155b2214dfef70b607fb3051fea92e0
unsatisfying <824941284@qq.com>
2024-09-16 19:47:09 +0800

    add modelscaner

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PersistExt.py_func_op_extract import PyFuncOpExtract
 import argparse
 import re
 import os
+import sys
 
 pattern = r'^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)' \
               r'(-[0-9A-Za-z-]+(\.[0-9A-Za-z-]+)*)?' \
@@ -22,6 +23,8 @@ def main(args):
         py_func_op_extract = PyFuncOpExtract(version, path)
         py_func_op_extract.analyze_tensorflow_source()
         py_func_op_extract.get_results(args.target)
+    elif args.detect and args.model:
+        print(1)
     else:
         print("Invalid arguments, please provide path, version and target file path!")
         return
@@ -32,9 +35,23 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Analyze Tensorflow API or detect malicious Tensorflow models(i.e., h5 or saved_model).")
 
-    # group = parser.add_mutually_exclusive_group()
-    parser.add_argument("-p", "--path", help="Tensorflow source code path")
-    parser.add_argument("-v", "--version", help="Tensorflow version")
-    parser.add_argument("-t", "--target", help="Target file path")
+    group1 = parser.add_argument_group("Tensorflow API analysis group")
+    group1.add_argument("-p", "--path", help="Tensorflow source code path")
+    group1.add_argument("-v", "--version", help="Tensorflow version")
+    group1.add_argument("-t", "--target", help="Target file path")
+    
+    group2 = parser.add_argument_group("Malicious model detection group")
+    group2.add_argument("-d", "--detect", help="Model detection")
+    group2.add_argument("-m", "--model", help="Tensorflow model path (i.e., h5 or saved_model)")
+
     args = parser.parse_args()
+    
+    # Ensure group1 or group2 parameters are provided as a full set
+    if (args.path or args.version or args.target) and not (args.path and args.version and args.target):
+        print("Error: Arguments -p, -v, -t must be used together.")
+        sys.exit(1)
+    elif (args.detect or args.model) and not (args.detect and args.model):
+        print("Error: Arguments -d, -m must be used together.")
+        sys.exit(1)
+    
     main(args)
